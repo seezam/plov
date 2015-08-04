75b92178e7f693460184996af763c66aff107dc1
Vladimir Kubyshev <v.kubyshev@corp.mail.ru>
2015-08-05 00:44:36 +0300

    master: animated arrows

---
+++ plov.com/plov.com/controllers/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  plov.com
+//
+//  Created by v.kubyshev on 05/08/15.
+//  Copyright (c) 2015 plov.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *leftArrow;
+
+@property (weak, nonatomic) IBOutlet UIImageView *rightArrow;
+@end
---
+++ plov.com/plov.com/controllers/MainViewController.m
@@ -0,0 +1,47 @@
+//
+//  MainViewController.m
+//  plov.com
+//
+//  Created by v.kubyshev on 05/08/15.
+//  Copyright (c) 2015 plov.com. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [UIView animateWithDuration:1 animations:^{
+            self.leftArrow.alpha = 0;
+            self.rightArrow.alpha = 0;
+        }];
+    });
+}
+
+@end
