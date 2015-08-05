2e23b14e9b0e4b4815a3c1524b1052541667a541
Vladimir Kubyshev <v.kubyshev@corp.mail.ru>
2015-08-06 02:31:45 +0300

    develop: custom launch screen

---
+++ plov.com/plov.com/controllers/StartViewController.h
@@ -0,0 +1,13 @@
+//
+//  StartViewController.h
+//  plov.com
+//
+//  Created by v.kubyshev on 06/08/15.
+//  Copyright (c) 2015 plov.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StartViewController : UIViewController
+
+@end
