dc5609c02f3e060356ad5599c26edab03fa64ae4
Vladimir Kubyshev <v.kubyshev@corp.mail.ru>
2015-08-06 22:15:52 +0300

    mater: menu icons

---
+++ plov.com/plov.com/controllers/MenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.h
+//  plov.com
+//
+//  Created by v.kubyshev on 06/08/15.
+//  Copyright (c) 2015 plov.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+
+@end
---
+++ plov.com/plov.com/controllers/MenuViewController.m
@@ -0,0 +1,57 @@
+//
+//  MenuViewController.m
+//  plov.com
+//
+//  Created by v.kubyshev on 06/08/15.
+//  Copyright (c) 2015 plov.com. All rights reserved.
+//
+
+#import "MenuViewController.h"
+
+@interface MenuViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, strong) NSArray * items;
+@end
+
+@implementation MenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - TableView delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+@end
