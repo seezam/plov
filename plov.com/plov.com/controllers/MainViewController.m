98ef93b4a4fd88a38b4333b52deddbe542016407
Vladimir Kubyshev <v.kubyshev@corp.mail.ru>
2015-08-06 03:08:45 +0300

    master: intro view

--- plov.com/plov.com/controllers/MainViewController.m
+++ plov.com/plov.com/controllers/MainViewController.m
@@ -39,7 +39,7 @@
     int steps = 7;
     int step = 1;
     __block CGFloat duration = 1.5;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self arrowAnimateWithStep:step forSteps:steps withDuration:duration];
     });
 }
--- plov.com/plov.com/controllers/StartViewController.m
+++ plov.com/plov.com/controllers/StartViewController.m
@@ -17,6 +17,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIStoryboard * storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UIViewController *viewControllerToBeShown = [storyBoard instantiateViewControllerWithIdentifier:@"beginViewController"];
+        [SHARED_APP.window addSubview:viewControllerToBeShown.view];
+        viewControllerToBeShown.view.alpha = 0;
+        
+        [UIView animateWithDuration:0.5 animations:^{
+            self.view.alpha = 0;
+            viewControllerToBeShown.view.alpha = 1;
+        } completion:^(BOOL finished) {
+            SHARED_APP.window.rootViewController = viewControllerToBeShown;
+        }];
+        
+    });
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    UIStoryboard * storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *viewControllerToBeShown = [storyBoard instantiateViewControllerWithIdentifier:@"beginViewController"];
+    SHARED_APP.window.rootViewController = viewControllerToBeShown;
 }
 
 - (void)didReceiveMemoryWarning {
